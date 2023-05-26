f22305e74892d82668d3448540e39dd93b0e033d
moreshwarborse <134684632+moreshwarborse@users.noreply.github.com>
2023-05-26 13:27:40 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n,fact=1;
+    cout<<"Enter Number:";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    
+        fact=i*fact;
+       { cout<<""<<fact;
+    }
+    return 0;
+}

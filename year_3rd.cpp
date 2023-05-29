cfaf66bea343847d9d7ddf34ccaeabb2808cc61f
moreshwarborse <134684632+moreshwarborse@users.noreply.github.com>
2023-05-29 21:31:22 +0530

    Update year_3rd.cpp

--- year_3rd.cpp
+++ year_3rd.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 using namespace std;
-int main()
+void year(int b)
 {
     int x,y,w,d;
     int t=365;
+
+    y=b/t;    
+    w=(b%t)/7;
+    d=(b%t)%7;
+    cout<<"It is "<<y<<" year, "<<w<<" week, "<<d<<" day." ;
+}
+int main()
+{
+    int x;
     cout<<"Enter days:";
     cin>>x;
 
-    y=x/t;    
-    w=(x%t)/7;
-    d=(x/t)%7;
-    cout<<"It is "<<y<<" year, "<<w<<" week, "<<d<<" day." ;
-
+    year(x);
+    return 0;
 }

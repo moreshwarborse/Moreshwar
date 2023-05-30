2822cd491f3ddd5130cacb3f6bca0e0239085dff
moreshwarborse <134684632+moreshwarborse@users.noreply.github.com>
2023-05-30 20:39:36 +0530

    Update ASCII_4th.cpp

--- ASCII_4th.cpp
+++ ASCII_4th.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main()
 {
-    char v;
+    char v,a,b,c,d;
     cout<<"Enter value:";
     cin>>v;
 
     cout<<"ASCII Number is:"<<int(v);
+    cout<<"\nSize of int:"<<sizeof((int) a);
+    cout<<"\nSize of char:"<<sizeof((char) b);
+    cout<<"\nSize of float:"<<sizeof((float) c);
+    cout<<"\nSize of double:"<<sizeof((double) b);
+
+    return 0;
 }

5c953ca2b8725775a4b9cd344708a6b650b254e8
moreshwarborse <134684632+moreshwarborse@users.noreply.github.com>
2023-06-01 13:23:33 +0530

    Add files via upload

---
+++ operater_6th.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=20;
+    cout<<"\n\na<b: "<< (a<b);
+    cout<<"\na>b: "<< (a>b);
+    cout<<"\na==b: "<< (a==b);
+    return 0;
+}
